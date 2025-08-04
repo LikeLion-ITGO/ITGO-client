c37b082ba6095ae799cb3a70412f82f96eecfbc8
kyinn1307 <shruddls1307@gmail.com>
2025-08-05 06:40:04 +0900

    feat: implement header with sidebar trigger icon (#2)

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,18 @@
 import MainLayout from "@/components/layouts/MainLayout";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/layouts/AppSidebar";
+import ChevronDown from "@/assets/icons/home/chevron-down.svg?react";
 
 export default function Home() {
   return (
     <MainLayout>
       <SidebarProvider>
         <AppSidebar />
-        <header className="w-full flex flex-row justify-between py-[14px]">
-          헤더
+        <header className="w-full h-11 flex flex-row items-center justify-between py-[14px]">
+          <div className="flex flex-row items-center text-xl font-semibold gap-[6px]">
+            공릉 1동
+            <ChevronDown />
+          </div>
           <SidebarTrigger />
         </header>
       </SidebarProvider>
