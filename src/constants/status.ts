559793289d8349e9d6e918525bbd65ee5ded8f37
kyinn1307 <shruddls1307@gmail.com>
2025-08-07 03:09:41 +0900

    fix: separate sharing status into give and receive (#3)

---
+++ src/constants/status.ts
@@ -0,0 +1,12 @@
+export enum GiveShareStatus {
+  NO_REQUEST = "NO_REQUEST", // 나눔 관련 아무 일도 없음
+  RECEIVED_REQUEST = "RECEIVED_REQUEST", // 누군가 나에게 나눔을 요청함
+  MATCHING_IN_PROGRESS = "MATCHING_IN_PROGRESS", // 나눔 요청을 검토 중 or 수락 중
+  SHARING_CONFIRMED = "SHARING_CONFIRMED", // 요청을 수락하고 나눔 매칭이 완료됨
+}
+
+export enum ReceiveShareStatus {
+  NO_REQUEST = "NO_REQUEST", // 나눔 요청을 한 적 없음
+  MATCHING_IN_PROGRESS = "MATCHING_IN_PROGRESS", // 나눔 요청을 보낸 상태 (수락 기다리는 중)
+  SHARING_CONFIRMED = "SHARING_CONFIRMED", // 요청이 수락되어 매칭 완료됨
+}
