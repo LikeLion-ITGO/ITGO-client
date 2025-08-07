6ac08792e7dab734f5df40345cab8311d29a5843
조아 <parkjoah@naver.com>
2025-08-07 18:41:23 +0900

    feat: ShareList category filter (#7)

--- src/components/shareListPage/ShareCategoryFilter.tsx
+++ src/components/shareListPage/ShareCategoryFilter.tsx
@@ -1,4 +1,12 @@
-export const ShareCategoryFilter = () => {
+interface ShareCategoryFilterProps {
+  selectedCat: string;
+  catHandler: (category: string) => void;
+}
+
+export const ShareCategoryFilter = ({
+  selectedCat,
+  catHandler,
+}: ShareCategoryFilterProps) => {
   const categories = ["전체", "유제품", "육류", "어류", "채소", "음식"];
 
   return (
@@ -9,8 +17,13 @@ export const ShareCategoryFilter = () => {
       <div className="flex gap-2 overflow-x-auto scrollbar-hide whitespace-nowrap -mx-[20px] px-[20px]">
         {categories.map((cat) => (
           <button
-            className={`px-3 py-2 rounded-[48px] shrink-0  body-03 bg-[#fff] text-[#8F9498]`}
+            className={`px-3 py-2 rounded-[48px] shrink-0  body-03 ${
+              cat === selectedCat
+                ? "bg-[#3CADFF] text-[#fff]"
+                : "bg-[#fff] text-[#8F9498]"
+            }`}
             type="button"
+            onClick={() => catHandler(cat)}
           >
             {cat}
           </button>
--- src/pages/ShareListPage.tsx
+++ src/pages/ShareListPage.tsx
@@ -3,8 +3,12 @@ import backIcon from "@/assets/icons/back.svg";
 import { ChevronDown } from "lucide-react";
 import { ShareCategoryFilter } from "@/components/shareListPage/ShareCategoryFilter";
 import { ShareProductList } from "@/components/shareListPage/ShareProductLIst";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const ShareListPage = () => {
+  const [selectedCat, setSelectedCat] = useState("전체");
+  const navigate = useNavigate();
   return (
     <MainLayout>
       <div className="bg-[#F5F7FA]">
@@ -13,13 +17,17 @@ export const ShareListPage = () => {
             src={backIcon}
             alt="<"
             className="absolute left-0 top-1/2 -translate-y-1/2"
+            onClick={() => navigate(-1)}
           />
           <div className="flex flex-row items-center text-xl font-semibold gap-[6px]">
             공릉 1동
             <ChevronDown />
           </div>
         </header>
-        <ShareCategoryFilter />
+        <ShareCategoryFilter
+          catHandler={setSelectedCat}
+          selectedCat={selectedCat}
+        />
         <div className="flex mb-5 mt-8 gap-[6px] text-[14px] text-[#5F6165] font-semibold leading-[100%] tracking-[-0.4px] items-center">
           최신순
           <ChevronDown className="w-[10px]" />
