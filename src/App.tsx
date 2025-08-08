d3c5e0d882711d1ecaf2fad45cd81cf8ff4144bc
kyinn1307 <shruddls1307@gmail.com>
2025-08-08 15:15:22 +0900

    feat: implement match succes page (#8)

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { ROUTES } from "./constants/routes";
 
 import Home from "./pages/home";
 import Manage from "./pages/manage";
+import { Success } from "./pages/success";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={ROUTES.HOME} element={<Home />} />
         <Route path={ROUTES.MANAGE} element={<Manage />} />
+        <Route path={ROUTES.SUCCESS} element={<Success />} />
       </Routes>
     </BrowserRouter>
   );
