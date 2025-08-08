424148858713ba69671421263b86f22ea8f8e460
조아 <parkjoah@naver.com>
2025-08-08 13:35:11 +0900

    feat: Implement UI - storeInfoPage (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ROUTES } from "./constants/routes";
 
 import Home from "./pages/home";
+import { StoreInfoPage } from "./pages/StoreInfoPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.STORE_INFO} element={<StoreInfoPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,6 @@
 export const ROUTES = {
   HOME: "/",
+  STORE_INFO: "/store-info/:storeId",
 } as const;
 
 export type RouteKey = keyof typeof ROUTES;
