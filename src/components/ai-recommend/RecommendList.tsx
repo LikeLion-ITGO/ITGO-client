7da79f81cfa2d3f57ed5d861793f78a8bc7fd95e
kyinn1307 <shruddls1307@gmail.com>
2025-08-10 00:06:58 +0900

    feat: add 'sent reqeust card item' into ai recommend list

--- src/components/ai-recommend/RecommendList.tsx
+++ src/components/ai-recommend/RecommendList.tsx
@@ -1,7 +1,13 @@
+import { SentRequestCardItem } from "../manage/SentRequestCardItem";
+
 export const RecommendList = () => {
   return (
     <div className="flex flex-col gap-4">
-      {/* 카드 리스트 넣을건데, manage에서 pending reqeust 카드 넣으면 될 듯 */}
+      <SentRequestCardItem />
+      <SentRequestCardItem />
+      <SentRequestCardItem />
+      <SentRequestCardItem />
+      <SentRequestCardItem />
     </div>
   );
 };
