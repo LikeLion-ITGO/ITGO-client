0bc51bff18a47ecf489599b38afe0b60c049ec58
조아 <parkjoah@naver.com>
2025-08-19 10:53:15 +0900

    feat: add Web Share API to ShareBox (#16)

--- src/components/home/bottom/ShareBox.tsx
+++ src/components/home/bottom/ShareBox.tsx
@@ -1,6 +1,21 @@
 import shareIcon from "../../../assets/icons/home/shareBtn.svg";
 
 export const ShareBox = () => {
+  const handleShare = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: "잇고 공유하기",
+          text: "잇고가 마음에 드셨다면 주변 사장님들께도 소개해 주세요!",
+          url: window.location.href,
+        });
+      } catch (err) {
+        console.error("공유 취소/에러:", err);
+      }
+    } else {
+      alert("이 브라우저는 공유 기능을 지원하지 않아요 😢");
+    }
+  };
   return (
     <div className="rounded-[24px] border border-[#F5F7FA] bg-white shadow-[0_2px_12px_0_rgba(0,0,0,0.05)] mb-10 p-[20px] justify-between flex flex-row h-[133px]">
       <div>
@@ -13,7 +28,10 @@ export const ShareBox = () => {
           {`함께 쓰면 더 편해져요 :)`}
         </div>
       </div>
-      <div className="w-[48px] h-[48px] bg-[#D5EDFF] flex items-center justify-center  rounded-[76px] shadow-[0_2px_12px_0_rgba(0,0,0,0.05)]">
+      <div
+        className="w-[48px] h-[48px] bg-[#D5EDFF] flex items-center justify-center  rounded-[76px] shadow-[0_2px_12px_0_rgba(0,0,0,0.05)]"
+        onClick={handleShare}
+      >
         <img
           src={shareIcon}
           alt="share"
